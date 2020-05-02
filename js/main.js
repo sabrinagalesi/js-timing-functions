f45828685130a624049c86aa982cc2ec7364a609
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-05-02 09:45:45 +0200

    Preparato ambiente

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi. 
+Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. 
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati 
+
+Fare un alert con 5 numeri casuali
+Fare una function con dentro un prompt che si ripete per 5 volte
+Fare un setTimeOut che avvii la funzione dopo 30 secondi*/
+
