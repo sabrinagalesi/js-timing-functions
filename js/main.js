a5d8e211aeaa92a30b8eda9e6fc03e9134ee05f3
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-05-02 13:01:52 +0200

    Iniziato esercizio

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ Fare un alert con 5 numeri casuali
 Fare una function con dentro un prompt che si ripete per 5 volte
 Fare un setTimeOut che avvii la funzione dopo 30 secondi*/
 
+function memoria() {
+    var numeriInseriti = []; //Array per i numeri che inserirà l'utente
+    for(var x = 0; x < 5; x++) {
+        var richiesta = parseInt(prompt("Digita uno dei numeri che hai visto prima!"));
+        numeriInseriti.push(richiesta);
+        console.log(numeriInseriti);
+    }
+}
+
+
+var listaNumeri = []; //Array per i numeri casuali da memorizzare
+
+for(var i = 0; i < 5; i++) { //Qui genero per 5 volte dei numeri casuali e li pusho nell'array listaNumeri
+listaNumeri.push(Math.floor(Math.random() * 30) + 1);
+}
+
+console.log(listaNumeri);
+
+
+alert("Memorizza i seguenti numeri! " + listaNumeri); //Faccio un alert che permetta di visualizzare i numeri da memorizzare
+
+var secondi = 30 * 1000; //converto i millisecondi in secondi
+
+setTimeout(memoria, secondi); //settimeout
+
+
+
+
+
+
+
